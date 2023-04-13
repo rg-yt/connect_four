1d80863bf5a0ef75c4ba0bf19a1f156f652d47d0
rg-yt <graytd1996@gmail.com>
2023-04-13 19:56:23 -0400

    Add test for an update board method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,27 @@
 require 'board'
 
 describe Board do
-  subject(:board) { described_class.new }
-  it 'initalizes' do
-    expect(:board).to_not be_nil
-  end 
+  subject(:board) {described_class.new()}
+  describe '#update_board' do
+    it 'changes the bottom character in the column to player input' do
+      player_input = 1
+      character = 'O'
+      expect{ board.update_board(player_input, character) }.to change{ board.values[5][1] }.to(character)
+    end
+
+    context 'when sent multiple characters' do
+      let(:character) {'o'}
+      let(:player_input) {1}
+      before do
+        player_input = 1
+        board.update_board(player_input,character) # add one 'o' to column 1
+         # add second 'o' to column 1
+      end
+      it 'changes the top of the stack' do
+        result = board.update_board(player_input,character)
+        expect(result).to eq(character)
+      end 
+      it 'changes'
+    end
+  end
 end
