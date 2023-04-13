d97955d30089cd2c4f7b348edd64bbcec5e92cc2
rg-yt <graytd1996@gmail.com>
2023-04-13 19:56:47 -0400

    Add method to update board

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,26 @@
+
 class Board
+  attr_reader :values
   
+  def initialize(values = Array.new(6){ Array.new(7)})
+    @values = values
+  end
+
+  def update_board(player_input, character)
+    values.reverse.map do |row|
+      return row[player_input] = character if row[player_input].nil?
+    end
+  end
+
+  def to_s
+    values.each { |row| p row }
+  end
 end
+board = Board.new
+board.update_board(1,'o')
+board.update_board(2,'o')
+board.update_board(2,'x')
+board.update_board(2,'x')
+
+
+board.to_s
