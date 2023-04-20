86ec9c85b429d315535414ca76377628aa41eca8
rg-yt <graytd1996@gmail.com>
2023-04-19 21:06:45 -0400

    Adjust board update to return coordinates

--- lib/board.rb
+++ lib/board.rb
@@ -6,31 +6,32 @@ class Board
     @values = values
   end
 
-  def update_board(player_input, character)
-    values.reverse.map do |row|
-      return row[player_input] = character if row[player_input].nil?
+  def update_board(column, character)
+    values.reverse.map.with_index do |row, index|
+      if row[column].nil?
+        row[column] = character
+        return [index, column]
+      end
     end
   end
 
-  def end_game?(player_input, character)
-    win_across?(player_input, character) || win_diagonally?(player_input, character) || win_up_down?(player_input, character)
+  def end_game?(coordinates, character)
+    win_across?(coordinates, character) || win_diagonally?(coordinates, character) || win_up_down?(coordinates, character)
   end
 
-  def win_up_down?(player_input, character, rows = [0, 1, 2, 3])
+  def win_up_down?(coordinates, character, rows = [0, 1, 2, 3])
     return false if rows.last == 6
-    return true if rows.all? { |row| values[row][player_input] == character }
+    return true if rows.all? { |row| values[row][coordinates.last] == character }
 
-    win_up_down?(player_input, character, rows.map { |row| row + 1 })
+    win_up_down?(coordinates, character, rows.map { |row| row + 1 })
   end
 
-  def win_across?(player_input, character)
-    values.any? do |row|
-      row.join('').include?(character * 4) unless row[player_input].nil?
-    end
+  def win_across?(coordinates, character)
+    values.reverse[coordinates.first].join('').include?(character * 4)
   end
 
   def win_diagonally?(player_input, character)
-  
+    
   end
 
   def pretty_print
@@ -46,6 +47,7 @@ class Board
     puts '-----------------------------------'
   end
 end
-# board = Board.new
-# p board.values
-# board.pretty_print
+board = Board.new
+p board.update_board(1, 'x')
+p board.update_board(1, 'x')
+
