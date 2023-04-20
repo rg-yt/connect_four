9bfd3911b59fa129252b37fbadd611f443425311
rg-yt <graytd1996@gmail.com>
2023-04-19 20:41:35 -0400

    Add player spec to test player

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,4 +1,13 @@
 require 'player'
 describe Player do
+  subject(:player) { described_class.new(1)}
+  it 'has a character value' do
+    value = '⚫'
+    player_value = player.char
+    expect(player.char).to eq(value)
+  end
 
+  it 'cannot be changed' do
+    expect{player.char = 10}.to raise_error(NoMethodError)
+  end 
 end
