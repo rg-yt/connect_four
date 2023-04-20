c9d77e8929b515b5f554e4c14b673d3c31c1ed70
rg-yt <graytd1996@gmail.com>
2023-04-19 23:10:47 -0400

    Add initialize tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,3 +1,15 @@
 require 'game'
 describe Game do
+  describe '#initialize' do
+    subject(:game) { described_class.new }
+    it 'has two players' do
+      players = game.instance_variable_get(:@players)
+      expect(players).to include(Player,Player)
+    end
+
+    it 'has a board' do
+      board = game.instance_variable_get(:@board)
+      expect(board).to be_a Board
+    end
+  end
 end
