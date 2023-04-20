be08fef255a034220722dd32c592d804067cc51d
rg-yt <graytd1996@gmail.com>
2023-04-19 20:41:59 -0400

    Add player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,15 @@
 class Player
-
+  attr_reader :char
+  def initialize(player_number)
+    @player_number = player_number
+    @char = get_char(player_number)
+  end
+  
+  def get_char(player_number)
+    if player_number == 1
+      '⚫'
+    else
+      '⚪'
+    end
+  end
 end
