17897adf28b7e7df5ad5bb47690d981f921499a7
rg-yt <graytd1996@gmail.com>
2023-04-19 20:40:18 -0400

    Add up-down and left-right win checks

--- lib/board.rb
+++ lib/board.rb
@@ -12,6 +12,27 @@ class Board
     end
   end
 
+  def end_game?(player_input, character)
+    win_across?(player_input, character) || win_diagonally?(player_input, character) || win_up_down?(player_input, character)
+  end
+
+  def win_up_down?(player_input, character, rows = [0, 1, 2, 3])
+    return false if rows.last == 6
+    return true if rows.all? { |row| values[row][player_input] == character }
+
+    win_up_down?(player_input, character, rows.map { |row| row + 1 })
+  end
+
+  def win_across?(player_input, character)
+    values.any? do |row|
+      row.join('').include?(character * 4) unless row[player_input].nil?
+    end
+  end
+
+  def win_diagonally?(player_input, character)
+  
+  end
+
   def pretty_print
     values.each do |row|
       row.each do |token|
@@ -25,6 +46,6 @@ class Board
     puts '-----------------------------------'
   end
 end
-board = Board.new
-
-board.pretty_print
+# board = Board.new
+# p board.values
+# board.pretty_print
